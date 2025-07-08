26a252dc1ed05b72d36f119a0c6416145c736145
Marcos Silva <marcos.silva79@gmail.com>
2025-07-08 10:57:15 -0400

    chore: retrieve aws related info from env variables

---
+++ src/main/java/com/msilva/qrgenpro/QrgenproApplication.java
@@ -0,0 +1,13 @@
+package com.msilva.qrgenpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrgenproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrgenproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/msilva/qrgenpro/QrgenproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msilva.qrgenpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrgenproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
