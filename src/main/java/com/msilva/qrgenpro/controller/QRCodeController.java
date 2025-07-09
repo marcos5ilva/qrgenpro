8c3d984357dec483381dc77c29bf9df70f44f160
Marcos Silva <marcos.silva79@gmail.com>
2025-07-09 19:05:22 -0400

    feat: add log4j, fix postmapping, improve Dockerfile

--- src/main/java/com/msilva/qrgenpro/controller/QRCodeController.java
+++ src/main/java/com/msilva/qrgenpro/controller/QRCodeController.java
@@ -1,16 +1,16 @@
 package com.msilva.qrgenpro.controller;
 
-import com.google.zxing.WriterException;
 import com.msilva.qrgenpro.dto.QrCodeGenerateRequest;
 import com.msilva.qrgenpro.dto.QrCodeGenerateResponse;
 import com.msilva.qrgenpro.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import javax.swing.text.html.parser.Entity;
-import java.io.IOException;
 
 @RestController
 @RequestMapping("/qrcode")
@@ -18,16 +18,20 @@ public class QRCodeController {
 
     private final QrCodeGeneratorService qrCodeGeneratorService;
 
+    private static final Logger logger = LogManager.getLogger(QRCodeController.class);
+
     public QRCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
         this.qrCodeGeneratorService = qrCodeGeneratorService;
     }
 
 
+    @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
         try {
             var response = qrCodeGeneratorService.generateAndUploadQrCode(request.text());
             return ResponseEntity.ok(response);
         } catch (Exception e) {
+            logger.error("Error generating QR code for text:{}", request.text(), e);
             return ResponseEntity.internalServerError().build();
         }
     }
